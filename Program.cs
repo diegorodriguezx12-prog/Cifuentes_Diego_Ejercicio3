fb33685734d044aa6e10a8c46f493bf891663674
Diego Cifuentes <diego.rodriguezx12@outlook.es>
2026-02-22 16:19:24 -0600

    Proyecto creado

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("=== Sistema de Facturacion con Control Antifraude ===");
+Console.WriteLine("Seleccione el tipo de cliente");
+Console.WriteLine("Estudiante(1) -- Docente(2) -- Administrativo(3) -- Externo(4)");
+Console.Write("Opción: ");
